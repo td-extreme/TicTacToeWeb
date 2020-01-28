b325c7b7fc3dee50082ca36d4a8a21a6c94fc89b
Ian Gagorik <igagorik@8thlight.com>
2020-01-28 09:46:06 -0600

    Add tsconfig.json to simplify ts build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,9 @@
 
 set -e
 
+./node_modules/typescript/bin/tsc
+
 cd ./src
 dotnet clean
 dotnet restore
 dotnet build
-
-cd ./Site/ts
-../../../node_modules/typescript/bin/tsc game.ts main.ts --outFile ../js/main.js
